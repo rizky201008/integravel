a5381f1f3b0c16b59d452b2d71235df96e23718d
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-10-27 23:25:03 +0700

    modify app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,18 +4,21 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    @stack('header')
+    @vite('resources/css/app.css')
+    @vite('resources/sass/app.scss')
 
-    <title>@stack('title', env('APP_NAME'))</title>
+    <title>{{ $title ?? env('APP_NAME') }}</title>
 </head>
 
-<body>
-    @yield('content')
+<body data-bs-theme="dark">
+    @include('components.navbar')
+    <div class="content" style="margin-top: 3.4rem;">
+        @yield('content')
+    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+    @vite('resources/js/app.js')
 </body>
 
 </html>
