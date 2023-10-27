5b782bb2689ea71cb889a026bb4826994928b109
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-10-27 23:23:54 +0700

    create routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Models\Rice;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'pages.home', ['title' => 'Beranda'])->name('home');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products');
+Route::get('/update/{id}', [ProductController::class, 'updateScreen'])->name('product-update');
+Route::post('/update/{id}', [ProductController::class, 'update'])->name('product-update-post');
+Route::delete('/delete/{id}', [ProductController::class, 'delete'])->name('product-delete');
+Route::get('/add',[ProductController::class,'addScreen'])->name('add-screen');
+Route::post('/add',[ProductController::class,'add'])->name('add-screen-post');
