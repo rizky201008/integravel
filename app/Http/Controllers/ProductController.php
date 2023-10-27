bbdb9cc337c4a28b65d1ec5e5d3d3e0c190e9262
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-10-27 23:23:39 +0700

    create new function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,8 +7,65 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function allProducts()
+    public function index()
     {
-        return response()->json(Rice::all());
+        return view('pages.products', ['data' => Rice::paginate(15), 'title' => 'Produk', 'no' => 1]);
+    }
+    public function updateScreen($id)
+    {
+        $data = Rice::findOrFail($id);
+        return view('pages.update', ['data' => $data, 'title' => 'Update ' . $data->merek, 'id' => $id]);
+    }
+    public function addScreen()
+    {
+        return view('pages.add', ['title' => "Tambah Produk"]);
+    }
+    public function add(Request $request)
+    {
+        $request->validate([
+            'merek' => 'required',
+            'harga' => 'required|numeric|gt:0',
+            'harga_grosir' => 'required|numeric|gt:0',
+            'kilo' => 'required|numeric|gt:0',
+            'stok' => 'required|numeric|gt:0',
+        ]);
+        if (
+            Rice::create([
+                'merek' => $request->merek,
+                'harga' => $request->harga,
+                'harga_grosir' => $request->harga_grosir,
+                'kilo' => $request->kilo,
+                'stok' => $request->stok
+            ])
+        ) {
+            return redirect(route('products'));
+        }
+    }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'merek' => 'required',
+            'harga' => 'required|numeric|gt:0',
+            'harga_grosir' => 'required|numeric|gt:0',
+            'kilo' => 'required|numeric|gt:0',
+            'stok' => 'required|numeric|gt:0',
+        ]);
+
+        if (
+            Rice::find($id)->update([
+                'merek' => $request->merek,
+                'harga' => $request->harga,
+                'harga_grosir' => $request->harga_grosir,
+                'kilo' => $request->kilo,
+                'stok' => $request->stok
+            ])
+        ) {
+            return redirect(route('products'));
+        }
+    }
+    public function delete(Request $request, $id)
+    {
+        Rice::find($id)->delete();
+        return back();
     }
 }
